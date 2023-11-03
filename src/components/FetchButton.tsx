1b6ac833fb08cd8480b0cdcc90cf7d225054dfba
oeyoews <jyao4783@gmail.com>
2023-11-03 22:51:20 +0800

    update

--- src/components/FetchButton.tsx
+++ src/components/FetchButton.tsx
@@ -9,22 +9,8 @@ function FetchData() {
 
   const [username, setUserName] = useState("")
   const [version, setVersion] = useState("")
-
-  const addTiddler = (text, title) => {
-    fetch("http://0.0.0.0:8000/recipes/default/tiddlers/plasmo", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "x-requested-with": "TiddlyWiki"
-      },
-      body: JSON.stringify({
-        text,
-      })
-    }).then((res) => {
-      if (res.ok) notify(`${title} 导入成功`)
-      return res.json()
-    })
-  }
+  const [text, setText] = useState("")
+  const [title, setTitle] = useState("")
 
   useEffect(() => {
     fetch("http://0.0.0.0:8000/status")
@@ -37,22 +23,53 @@ function FetchData() {
       })
   }, [])
 
+
+  const addTiddler = () => {
+    fetch(`http://0.0.0.0:8000/recipes/default/tiddlers/${title}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-requested-with": "TiddlyWiki"
+      },
+      body: JSON.stringify({
+        creator: username,
+        text,
+      })
+    }).then((res) => {
+      if (res.ok) notify(`${title} 导入成功`)
+      return res.json()
+    })
+  }
+
+  const handleAddTiddler = (e) => {
+    e.preventDefault()
+    addTiddler()
+  }
+
   return (
-    <div className="">
+    <div>
       <div className="flex text-sm space-x-2 justify-center items-center">
         Username: <div className="underline"> {username}</div>
         Version: <div className="underline">{version}</div>
       </div>
 
-      <div className="my-2">
+      <form onSubmit={handleAddTiddler} className="my-2 space-y-2">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="bg-neutral-200 rounded w-full outline-none focus:outline-none mx-2 px-1 placeholder:text-gray-200"
+          placeholder="💡Title"
+          required
+        />
         <textarea
-          className="bg-neutral-200 appearance-none rounded w-full outline-none focus:outline-none mx-2 px-1 placeholder:text-gray-200"
-          placeholder="✍下面输入内容"
-          rows={1}></textarea>
-        <button onClick={() => addTiddler("test", 'title test')} className="bg-gray-400 rounded-sm p-1">
-          import
-        </button>
-      </div>
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="bg-neutral-200 rounded mx-2 px-1 py-2 w-full h-full max-h-[300px] my-1 text-base resize-none overflow-x-hidden overflow-y-auto outline-none whitespace-pre-wrap word-break"
+          placeholder="✍输入内容"
+          rows={1}
+          required></textarea>
+      </form>
       <ToastContainer
         position="top-right"
         autoClose={2000}
